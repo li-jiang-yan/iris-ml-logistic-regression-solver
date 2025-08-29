52ae070a181bbc25297fe30d658a79bcdde8b658
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-08-29 11:42:22 +0300

    Added the training of the model with LogisticRegression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 # Load dataset
 iris = datasets.load_iris()
@@ -13,3 +14,6 @@ X = StandardScaler().fit(X).transform(X)
 # Split data into train and test data
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
+# Train model
+clf = LogisticRegression(random_state=0)
+clf.fit(X_train, y_train)
